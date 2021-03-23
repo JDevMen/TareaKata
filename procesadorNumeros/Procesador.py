bbe985ed897d60c2cbc6fa4fa8f7dd9a64770aa9
JDevMen <jdmendoza_90@hotmail.com>
2021-03-23 02:05:19 -0500

    test obtener el promedio de los elementos 2 elementos (rojo)

--- procesadorNumeros/Procesador.py
+++ procesadorNumeros/Procesador.py
@@ -28,5 +28,5 @@ class Procesador:
                 respuesta.append(minimo)
                 respuesta.append(maximo)
                 respuesta.append(0)
-                
+
             return respuesta
