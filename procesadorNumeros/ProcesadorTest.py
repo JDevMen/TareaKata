51a241681ef5f9504c80f8b91f576f5326046d59
JDevMen <jdmendoza_90@hotmail.com>
2021-03-23 00:01:50 -0500

    test obtener num elementos con string vacio (rojo)

--- procesadorNumeros/ProcesadorTest.py
+++ procesadorNumeros/ProcesadorTest.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
+from Procesador import Procesador
+
 
 class ProcesadorTest(TestCase):
-    def test_procesador_text(self):
-        self.fail()
+    def test_procesador_String_Vacio(self):
+        self.assert_(Procesador().procesadorText(""), [0, -1, -1, -1], "Arreglo vacío")
