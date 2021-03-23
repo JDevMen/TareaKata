a89adf8861a851abf6c1d5140e0678745d229d23
JDevMen <jdmendoza_90@hotmail.com>
2021-03-23 00:25:19 -0500

    test obtener num elementos con string vacio (verde)

--- procesadorNumeros/Procesador.py
+++ procesadorNumeros/Procesador.py
@@ -1,3 +1,7 @@
 class Procesador:
-    def procesadorText(numeros):
-        pass
+    def procesadorText(self, numeros):
+        respuesta = []
+        if numeros == "":
+            respuesta = [0, 0, 0, 0]
+            print(respuesta)
+            return respuesta
--- procesadorNumeros/ProcesadorTest.py
+++ procesadorNumeros/ProcesadorTest.py
@@ -5,4 +5,4 @@ from Procesador import Procesador
 
 class ProcesadorTest(TestCase):
     def test_procesador_String_Vacio(self):
-        self.assertEqual(Procesador().procesadorText(""), ([0, -1, -1, -1]), "Arreglo vacío")
+        self.assertEqual(Procesador().procesadorText(""), [0, 0, 0, 0], "Arreglo vacío")
