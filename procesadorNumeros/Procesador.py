3002acfa2b6c7e63f4ec24654d3c4475235c82e1
JDevMen <jdmendoza_90@hotmail.com>
2021-03-23 01:28:00 -0500

    test obtener elemento más pequeno dos elementos (verde)

--- procesadorNumeros/Procesador.py
+++ procesadorNumeros/Procesador.py
@@ -9,7 +9,17 @@ class Procesador:
             lista = numeros.split(",")
 
             respuesta.append(len(lista))
-            respuesta.append(1)
+
+            if len(lista) == 1:
+                respuesta.append(int(lista[0]))
+            else:
+                elementoA = int(lista[0])
+                elementoB = int(lista[1])
+
+                if elementoA < elementoB:
+                    respuesta.append(elementoA)
+                else:
+                    respuesta.append(elementoB)
             respuesta.append(0)
             respuesta.append(0)
             return respuesta
