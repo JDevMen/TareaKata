5069414e02600f12606ce0a76ed78f2461501144
JDevMen <jdmendoza_90@hotmail.com>
2021-03-23 01:03:36 -0500

    test obtener num elementos con n numeros (rojo)

--- procesadorNumeros/ProcesadorTest.py
+++ procesadorNumeros/ProcesadorTest.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from Procesador import Procesador
+import random
 
 
 class ProcesadorTest(TestCase):
@@ -13,3 +14,15 @@ class ProcesadorTest(TestCase):
     def test_procesador_String_numero_elementos_dos_Numeros(self):
         self.assertEqual(Procesador().procesadorText("1,2"), [2, 0, 0, 0], "Arreglo con un número")
 
+    def test_procesador_String_numero_elementos_n_Numeros(self):
+        numElementos = random.randint(3, 10)
+        stringAProcesar = ""
+
+        if numElementos >= 1:
+            i = 1
+            stringAProcesar += "1"
+            while i < numElementos:
+                numRandom = random.randint(0, 10)
+                stringAProcesar += "," + str(numRandom)
+                i += 1
+        self.assertEqual(Procesador().procesadorText(stringAProcesar), [numElementos, 0, 0, 0], "Arreglo con un número")
