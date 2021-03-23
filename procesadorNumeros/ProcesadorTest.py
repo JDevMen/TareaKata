8bc51228c267fb445ce66769f51ec90e1fd1e73e
JDevMen <jdmendoza_90@hotmail.com>
2021-03-23 00:47:28 -0500

    test obtener num elementos con dos numeros (rojo)

--- procesadorNumeros/ProcesadorTest.py
+++ procesadorNumeros/ProcesadorTest.py
@@ -10,3 +10,6 @@ class ProcesadorTest(TestCase):
     def test_procesador_String_numero_elementos_un_Numero(self):
         self.assertEqual(Procesador().procesadorText("1"), [1, 0, 0, 0], "Arreglo con un número")
 
+    def test_procesador_String_numero_elementos_dos_Numeros(self):
+        self.assertEqual(Procesador().procesadorText("1,2"), [2, 0, 0, 0], "Arreglo con un número")
+
