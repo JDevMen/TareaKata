ecbcaa4b40f1d877b50b32b0b33b0d826f199009
JDevMen <jdmendoza_90@hotmail.com>
2021-03-23 01:41:44 -0500

    test obtener elemento más grande 1 elemento (rojo)

--- procesadorNumeros/ProcesadorTest.py
+++ procesadorNumeros/ProcesadorTest.py
@@ -49,10 +49,10 @@ class ProcesadorTest(TestCase):
         listaPrueba = ""
         self.assertEqual(Procesador().procesadorText(listaPrueba), [0, -1, -1, 0], "Arreglo vacio")
 
-    # def test_procesador_String_numero_encontrar_maximo_un_numero(self):
-    #     listaPrueba = "1"
-    #     self.assertEqual(Procesador().procesadorText(listaPrueba), [1, 1, 0, 0], "Arreglo vacio")
-    #
+    def test_procesador_String_numero_encontrar_maximo_un_numero(self):
+        listaPrueba = "1"
+        self.assertEqual(Procesador().procesadorText(listaPrueba), [1, 1, -1, 0], "Arreglo vacio")
+
     # def test_procesador_String_numero_encontrar_maximo_dos_numero(self):
     #     listaPrueba = "2,3"
     #     self.assertEqual(Procesador().procesadorText(listaPrueba), [2, 2, 0, 0], "Arreglo vacio")
